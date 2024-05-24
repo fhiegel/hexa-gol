3125c66041caa235c1576258a0ef1e46e9e4e0cb
Fabien HIEGEL <fabien.hiegel@gmail.com>
2024-05-24 23:15:40 +0200

    🎉 init: Démarrer un nouveau projet Java+Maven

---
+++ src/test/java/io/dedale/AppTest.java
@@ -0,0 +1,34 @@
+package io.dedale;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void run_application_displays_new_world() {
+        App.main(new String[]{});
+        assertThat(outContent.toString())
+                .isEqualTo("Hello World!" + System.lineSeparator());
+    }
+}
